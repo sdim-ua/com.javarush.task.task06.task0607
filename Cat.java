7ca99791093d185b5c2c91adb8eb225edbab20b3
sdim-ua <ingersol@yandex.ua>
2019-02-03 12:34:01 +0400

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,18 @@
+/* 
+Классовый счетчик
+*/
+
+public class Cat {
+    //напишите тут ваш код
+    static int catCount;
+    
+    public Cat(){
+        Cat.catCount++;
+    }
+    
+    
+
+    public static void main(String[] args) {
+
+    }
+}
